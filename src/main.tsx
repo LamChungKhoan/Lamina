56389d11e0d34dacd9d318614e2df4f288083021
Quang <vinhquangcontroller1@gmail.com>
2026-04-04 01:06:07 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import './index.css';
+import TestAI from './TestAI';
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-   import TestAI from "./TestAI";
-
-<TestAI />
+    <TestAI />
   </StrictMode>,
 );
