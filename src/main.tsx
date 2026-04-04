ab4ed74e183abc4e811c443792ec37f6570eed39
Quang <vinhquangcontroller1@gmail.com>
2026-04-04 12:43:34 +0700

    app run ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import TestAI from './TestAI';
+import App from './App'; // 👉 Import component App (giao diện chính)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TestAI />
+    <App /> {/* 👉 Render component App tại đây */}
   </StrictMode>,
 );
